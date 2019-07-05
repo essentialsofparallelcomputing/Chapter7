b06ae9fc9d05a592fcdc5232c3d842b1cab90066
brobey <brobey@lanl.gov>
2019-07-05 13:59:52 -0600

    Adding another high-level OpenMP optimization

--- HighLevelOpenMP_stencil/batch.sh
+++ HighLevelOpenMP_stencil/batch.sh
@@ -3,13 +3,21 @@
 export OMP_PLACES=cores;
 export OMP_CPU_BIND=true;
 
+echo "Running stencil_opt2"
 export OMP_NUM_THREADS=88; ./stencil_opt2
 echo ""
 echo ""
+echo "Running stencil_opt3"
 export OMP_NUM_THREADS=88; ./stencil_opt3
 echo ""
 echo ""
+echo "Running stencil_opt4"
 export OMP_NUM_THREADS=88; ./stencil_opt4
 echo ""
 echo ""
+echo "Running stencil_opt5"
 export OMP_NUM_THREADS=88; ./stencil_opt5
+echo ""
+echo ""
+echo "Running stencil_opt6"
+export OMP_NUM_THREADS=88; ./stencil_opt6
---
+++ HighLevelOpenMP_stencil/stencil_opt6.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "malloc2D.h"
+#include "timer.h"
+#include <omp.h>
+
+#define SWAP_PTR(xnew,xold,xtmp) (xtmp=xnew, xnew=xold, xold=xtmp)
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+int main(int argc, char *argv[])
+{
+   #pragma omp parallel
+      if (omp_get_thread_num() == 0) printf("Running with %d thread(s)\n",omp_get_num_threads());
+
+   struct timeval tstart_init, tstart_flush, tstart_stencil, tstart_total;
+   double init_time, flush_time, stencil_time, total_time;
+   int imax=2002, jmax = 2002;
+
+   double** xtmp;
+   double** x = malloc2D(jmax, imax);
+   double** xnew = malloc2D(jmax, imax);
+   int *flush = (int *)malloc(jmax*imax*sizeof(int)*10);
+
+   cpu_timer_start(&tstart_total);
+#pragma omp parallel
+   {
+      int thread_id = omp_get_thread_num();
+      int nthreads = omp_get_num_threads();
+      int jtsize = (jmax-2)/nthreads;
+      if (thread_id < (jmax-2)%nthreads) jtsize++;
+      int jltb = 1 + (jmax-2)/nthreads*thread_id+MIN((imax-2)%nthreads, thread_id);
+      int jutb = jltb + jtsize -1;
+      //printf("DEBUG -- thread_id %d imax %d jtsize %d jltb %d jutb %d\n",thread_id,imax,jtsize,jltb,jutb);
+
+      int ifmax = jmax*imax*10;
+      int iftsize = ifmax/nthreads;
+      if (thread_id < ifmax%nthreads) iftsize++;
+      int ifltb = ifmax/nthreads*thread_id+MIN(ifmax%nthreads, thread_id);
+      int ifutb = ifltb + iftsize -1;
+      //printf("DEBUG -- thread_id %d ifmax %d iftsize %d ifltb %d ifutb %d\n",thread_id,ifmax,iftsize,ifltb,ifutb);
+
+      int jltb0 = jltb;
+      if (thread_id ==0) jltb0--;
+      int jutb0 = jutb;
+      if (thread_id ==nthreads-1) jutb0++;
+      //printf("DEBUG -- thread_id %d jltb0 %d jutb0 %d\n",thread_id,jltb0,jutb0);
+
+      int kmin = jmax/2-5;
+      int kmax = jmax/2+5;
+      //printf("DEBUG -- thread_id %d kmin %d kmax %d\n",thread_id,kmin,kmax);
+      kmin = MAX(kmin,jltb);
+      kmax = MIN(kmax,jutb);
+      //printf("DEBUG -- thread_id %d kmin %d kmax %d\n",thread_id,kmin,kmax);
+
+      if (thread_id == 0) cpu_timer_start(&tstart_init);
+      for (int j = jltb0; j < jutb0; j++){
+         for (int i = 0; i < 0; i++){
+            xnew[j][i] = 0.0;
+            x[j][i] = 5.0;
+         }
+      }
+
+      for (int j = kmin; j < kmax; j++){
+         for (int i = imax/2 - 5; i < imax/2 -1; i++){
+            x[j][i] = 400.0;
+         }
+      }
+#pragma omp barrier
+      if (thread_id == 0) init_time += cpu_timer_stop(tstart_init);
+
+
+      for (int iter = 0; iter < 10000; iter++){
+         if (thread_id ==0) cpu_timer_start(&tstart_flush);
+         for (int l = ifltb; l < ifutb; l++){
+            flush[l] = 1.0;
+         }
+         if (thread_id == 0){
+            flush_time += cpu_timer_stop(tstart_flush);
+            cpu_timer_start(&tstart_stencil);
+         }
+         for (int j = jltb; j < jutb; j++){
+            for (int i = 1; i < imax-1; i++){
+               xnew[j][i] = ( x[j][i] + x[j][i-1] + x[j][i+1] + x[j-1][i] + x[j+1][i] )/5.0;
+            }
+         }
+#pragma omp barrier
+         if (thread_id == 0){
+            stencil_time += cpu_timer_stop(tstart_stencil);
+
+            SWAP_PTR(xnew, x, xtmp);
+            if (iter%1000 == 0) printf("Iter %d\n",iter);
+         }
+      }
+   } // end omp parallel
+   total_time += cpu_timer_stop(tstart_total);
+
+   printf("Timing is init %f flush %f stencil %f total %f\n",init_time,flush_time,stencil_time,total_time);
+
+}
