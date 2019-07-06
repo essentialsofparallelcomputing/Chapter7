ef8fd5509e7879016b987b758e8f72d8c516c78d
Bob Robey <brobey@lanl.gov>
2019-07-06 17:36:55 -0600

    Adding another simplification

---
+++ HelloOpenMP/HelloOpenMP_fix5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "omp.h"
+
+int main(int argc, char *argv[]){
+   printf("Goodbye slow serial world and Hello OpenMP!\n");
+   #pragma omp parallel
+   if (omp_get_thread_num() == 0) {
+      printf("  I have %d thread(s) and my thread id is %d\n",
+             omp_get_num_threads(), omp_get_thread_num());
+   }
+}
