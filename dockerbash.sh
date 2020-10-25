e0ea20b54035fabf9c4ba98fd0a94f7c0208d9a4
Bob Robey <brobey@lanl.gov>
2020-10-24 18:40:13 -0600

    Container fixes

--- dockerbash.sh
+++ dockerbash.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker run -it --entrypoint /bin/bash essentialsofparallelcomputing/chapter7
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+docker run -it --gpus all -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
+        --entrypoint /bin/bash essentialsofparallelcomputing/chapter7
