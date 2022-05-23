4d89f8fcd0deef37e4fd83a7209085bbaa162251
Bob Robey <brobey@lanl.gov>
2022-05-23 13:18:53 -0600

    Making timer.h C++ compatible

--- StreamTriad/timer.h
+++ StreamTriad/timer.h
@@ -2,6 +2,15 @@
 #define _TIMER_H
 #include <time.h>
 
+#ifdef __cpluscplus
+extern "C" {
+#endif
+
 void cpu_timer_start(struct timespec *tstart_cpu);
 double cpu_timer_stop(struct timespec tstart_cpu);
+
+#ifdef __cpluscplus
+}
+#endif
+
 #endif
